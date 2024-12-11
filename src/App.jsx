caf4f6471eac5424c257a1406681661cf0d54a1e
Carolina Devia <deviacaro96@gmail.com>
2024-12-11 19:39:50 +0100

    wip

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 function App() {
   return (
     <>
-      <Navbar></Navbar>
+      <Navbar/>
       <ItemListContainer greeting="¡Welcome to Plantasia!"></ItemListContainer>
     </>
   );
